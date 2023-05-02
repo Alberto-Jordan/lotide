6e9c2e55af0c6f69a01428f9a41f7dd959307e21
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-01 20:05:59 -0600

    head

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
