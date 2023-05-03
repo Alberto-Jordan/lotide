2cb7260004a36337d6c1db4dd72c6cb6a12f754b
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-03 14:54:45 -0600

    assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+  for (const key of obj1Keys) {
+    if (!object2.hasOwnProperty(key) || object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Logs a message to the console indicating whether or not the two objects are equal
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// Test cases
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject, longSleeveShirtObject); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
