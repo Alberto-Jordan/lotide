782a28e7b6c01272162273abb3d081e9b4eed4d2
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-08 13:03:43 -0600

    refactored

--- tail.js
+++ tail.js
@@ -1,24 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = require('./assertEqual');
+
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
   } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return array.slice(1);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const tail = function(array) {
-  return array.slice(1);
-};
-
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3); 
-
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
-assertEqual(tail([1]).toString(), [].toString());
-assertEqual(tail([]), undefined);
+module.exports = tail;
