1c1221c06a0ab44d71b42894e9f2df0be1315144
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-08 13:05:32 -0600

    refactored

---
+++ middle.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(arr) {
+  let middleElements = [];
+  const middleIndex = Math.floor(arr.length / 2);
+
+  if (arr.length <= 2) {
+    return middleElements;
+  } else if (arr.length % 2 === 0) {
+    middleElements.push(arr[middleIndex - 1], arr[middleIndex]);
+  } else {
+    middleElements.push(arr[middleIndex]);
+  }
+
+  return middleElements;
+};
+
+module.exports = middle;
