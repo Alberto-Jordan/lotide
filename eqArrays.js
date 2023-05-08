3e3a9c3092fc29e77bd2a03006ae3fd8e4d1c18b
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-08 13:05:02 -0600

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -10,8 +12,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
+module.exports = eqArrays;
