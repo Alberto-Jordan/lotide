884042a6fc9e70fdd5363b971158b7900f324c9d
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-08 13:02:28 -0600

    refactored

--- head.js
+++ head.js
@@ -1,17 +1,5 @@
 const assertEqual = require('./assertEqual');
 
-
-// const assertEqual = function(actual, expected) {
- // if (actual === expected) {
-  //  console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
- // } else {
- //   console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  // }
-// };
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
@@ -19,8 +7,3 @@ const head = function(array) {
     return array[0];
   }
 };
-
-//assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([1]), 1);
-// assertEqual(head([]), undefined);
